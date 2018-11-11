1b0a4f7d8a08d6d01a19e8bc9933995ea6b1fd89
John Maloney <maloney1@hotmail.com>
2018-11-11 20:05:51 +0000

    Adding the Messaging foundational pieces

---
+++ Messaging.Tests.Integration/QueueClientTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Messaging.Tests.Integration
+{
+    [TestClass]
+    public class QueueClientTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
