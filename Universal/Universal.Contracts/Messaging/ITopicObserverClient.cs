9c2c561ed2ec4a1abd599470f28503953f41644e
John Maloney <maloney1@hotmail.com>
2019-08-31 23:11:25 +0000

    Adding the topic interfaces

---
+++ Universal/Universal.Contracts/Messaging/ITopicObserverClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Universal.Contracts.Messaging
+{
+    public interface ITopicObserverClient
+    {
+        void RegisterForNotificationOf<TMessage>(Func<IMessage, Task> messageHandler);
+    }
+}
