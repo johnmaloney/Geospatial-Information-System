debb2539a7313a5edc79d342da2d301d753727cb
John Maloney <maloney1@hotmail.com>
2019-09-01 00:00:42 +0000

    Adding topics and queues

---
+++ Universal/Universal.Contracts/Messaging/Topics.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Universal.Contracts.Messaging
+{
+    public struct Topics
+    {
+        public const string GeneralInfo = "general-info";
+        public const string GeneralEvent = "general-event";
+    }
+
+    public struct Queues
+    {
+        public const string GeneralCommand = "general-command";
+    }
+}
