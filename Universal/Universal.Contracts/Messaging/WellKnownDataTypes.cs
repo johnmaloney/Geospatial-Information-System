ebcb4c4aaaaafcbe53d35a9fb34d20302cfae0be
John Maloney <maloney1@hotmail.com>
2019-09-14 23:10:44 +0000

    Addind the WKT names

---
+++ Universal/Universal.Contracts/Messaging/WellKnownDataTypes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Universal.Contracts.Messaging
+{
+    public class WellKnownDataTypes
+    {
+        public const string GeoJSON= "geojson";
+        public const string FileName = "filename";
+        public const string LayerName = "layername";
+    }
+}
