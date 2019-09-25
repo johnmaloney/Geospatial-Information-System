9d94c6615978d6eb2ebc9b32e2d8cb626e660b3c
John Maloney <maloney1@hotmail.com>
2019-09-25 03:07:04 +0000

    Adding the tile cache interface

---
+++ Universal/Universal.Contracts/Tiles/ITileCacheStorage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Universal.Contracts.Tiles
+{
+    public interface ITileCacheStorage<TTile>
+    {
+        TTile GetBy(int id);
+        void StoreBy(int id, TTile tile);
+        void Clear();
+    }
+}
