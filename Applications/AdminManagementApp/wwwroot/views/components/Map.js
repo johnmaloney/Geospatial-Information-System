963360e2c2b53d26a8b535dba3e1b0e8dfe5d343
John Maloney <maloney1@hotmail.com>
2019-09-25 04:16:47 +0000

    fixing map issue

--- Applications/AdminManagementApp/wwwroot/views/components/Map.js
+++ Applications/AdminManagementApp/wwwroot/views/components/Map.js
@@ -37,32 +37,7 @@ let Map = {
 
         var map = L.map('mapid');
         map.setView({ lat: 40, lng: -104 }, 10); 
-
-        var controlLayers = [];
-        layers.forEach(layer => {
-
-            //var url = layer.properties.find(function(element) {
-            //    return element.name === "TileAccessTemplate";
-            //});
-
-            //var mapboxUrl = url.value;
-
-            //var mapboxVectorTileOptions = {
-            //    rendererFactory: L.canvas.tile,
-            //    token: 'pk.eyJ1IjoiaXZhbnNhbmNoZXoiLCJhIjoiY2l6ZTJmd3FnMDA0dzMzbzFtaW10cXh2MSJ9.VsWCS9-EAX4_4W1K-nXnsA'
-            //};
-
-            //var mapboxPbfLayer = L.vectorGrid.protobuf(mapboxUrl, mapboxVectorTileOptions).addTo(map);
-
-            //var tileInfo = {};
-            //tileInfo[layer.name] = mapboxPbfLayer;
-
-            //controlLayers.push(mapboxPbfLayer);
-
-        });   
-
-             
-
+        
         var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
             attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
         });
