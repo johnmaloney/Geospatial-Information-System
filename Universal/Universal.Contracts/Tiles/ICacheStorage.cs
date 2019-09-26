cbc0f55cca73c5e41a58e43dd84ffea475ab38b0
John Maloney <maloney1@hotmail.com>
2019-09-26 03:51:09 +0000

    Adding generic cache interface

---
+++ Universal/Universal.Contracts/Tiles/ICacheStorage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Universal.Contracts.Tiles
+{
+    public interface ICacheStorage<TKey, TValue>
+    {
+        TValue GetBy(TKey id);
+        void StoreBy(TKey id, TValue tile);
+        void Clear();
+    }
+}
